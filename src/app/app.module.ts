74be16b120e116278685ee0b776ea1974c2f8e05
Deepak <deepucsait@gmail.com>
2021-03-20 20:54:39 +0530

    Home page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
